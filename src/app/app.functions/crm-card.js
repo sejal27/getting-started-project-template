147f2a1a5cfce1eb6ffac2430659c727d8e74d7a
Sejal <sejalrparikh@gmail.com>
2022-08-02 23:27:47 -0400

    let's just do something simple

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -5,71 +5,103 @@ exports.main = async (context = {}, sendResponse) => {
 
   // Make a all to ZenQuotes public API and fetch data
 
-  const getExternalData = async () => {
-    try {
-      const { data } = await axios.get("https://zenquotes.io/api/random");
-      const quoteSections = [
+  // const getExternalData = async () => {
+  //   try {
+  //     const { data } = await axios.get("https://zenquotes.io/api/random");
+  //     const quoteSections = [
+  //       {
+  //         "type": "tile",
+  //         "body": [
+  //           {
+  //             "type": "text",
+  //             "format": "markdown",
+  //             "text": "**Hello " + firstname + ", Here's your quote for the day**!"
+  //           },
+  //           {
+  //             "type": "text",
+  //             "format": "markdown",
+  //             "text": "**Quote**:" + data[0].q
+  //           },
+  //           {
+  //             "type": "text",
+  //             "format": "markdown",
+  //             "text": "**Author**:" + data[0].a
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         "type": "button",
+  //         "text": "Get new quote",
+  //         "onClick": {
+  //           "type": "SERVERLESS_ACTION_HOOK",
+  //           "serverlessFunction": "crm-card"
+  //         }
+  //       }
+  //     ];
+  //     return quoteSections;
+  //   } catch (error) {
+  //     const quoteSections = [
+  //       {
+  //         "type": "alert",
+  //         "variant": "error"
+  //         "title": "Error fetching new quote",
+  //         "body": {
+  //           "type": "text",
+  //           "text": error.message
+  //         }
+  //       }
+  //     ];
+  //     return quoteSections;
+  //   }
+  // };
+  
+  // const quotes = await getExternalData();
+  
+  // const introMessage = [
+  //   {
+  //     type: "text",
+  //     text: "An example of a CRM card extension that displays data from Hubspot, uses ZenQuotes public API to display daily quote, and demonstrates custom actions using serverless functions.",
+  //   },
+  //   {
+  //     type: "divider",
+  //     distance: "small",
+  //   }
+  // ];
+
+  // const sections = [...introMessage, ...quotes];
+
+  const { data } = await axios.get("https://zenquotes.io/api/random");
+
+  const sections = [
+    {
+      "type": "tile",
+      "body": [
         {
-          "type": "tile",
-          "body": [
-            {
-              "type": "text",
-              "format": "markdown",
-              "text": "**Hello " + firstname + ", Here's your quote for the day**!"
-            },
-            {
-              "type": "text",
-              "format": "markdown",
-              "text": "**Quote**:" + data[0].q
-            },
-            {
-              "type": "text",
-              "format": "markdown",
-              "text": "**Author**:" + data[0].a
-            }
-          ]
+          "type": "text",
+          "format": "markdown",
+          "text": "**Hello " + firstname + ", Here's your quote for the day**!"
         },
         {
-          "type": "button",
-          "text": "Get new quote",
-          "onClick": {
-            "type": "SERVERLESS_ACTION_HOOK",
-            "serverlessFunction": "crm-card"
-          }
-        }
-      ];
-      return quoteSections;
-    } catch (error) {
-      const quoteSections = [
+          "type": "text",
+          "format": "markdown",
+          "text": "**Quote**:" + data[0].q
+        },
         {
-          "type": "alert",
-          "variant": "error"
-          "title": "Error fetching new quote",
-          "body": {
-            "type": "text",
-            "text": error.message
-          }
+          "type": "text",
+          "format": "markdown",
+          "text": "**Author**:" + data[0].a
         }
-      ];
-      return quoteSections;
-    }
-  };
-  
-  const quotes = await getExternalData();
-  
-  const introMessage = [
-    {
-      type: "text",
-      text: "An example of a CRM card extension that displays data from Hubspot, uses ZenQuotes public API to display daily quote, and demonstrates custom actions using serverless functions.",
+      ]
     },
     {
-      type: "divider",
-      distance: "small",
+      "type": "button",
+      "text": "Get new quote",
+      "onClick": {
+        "type": "SERVERLESS_ACTION_HOOK",
+        "serverlessFunction": "crm-card"
+      }
     }
   ];
-
-  const sections = [...introMessage, ...quotes];
-
   sendResponse({
     sections,
   });
